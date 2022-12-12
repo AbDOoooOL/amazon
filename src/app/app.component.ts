c87afcf45d12ebc16a6f93c62921ce730940c0bd
Abdul Hade Mobayed <abdalhademobayed@gmail.com>
2022-12-12 23:38:10 +0300

    hello

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amzn-root',
+  templateUrl: './app.component.html',
+  
+})
+export class AppComponent {
+  title: string = 'welcome to this page';
+
+  changeImage(): void{
+
+  }
+}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amzn-products',
+  templateUrl: './product-list.component.html',
+
+})
+export class ProductListComponant {
+
+  pageTitle: string = "Product List";
+
+  imageW: number = 100;
+  imageMar: number = 5;
+  
+  myPhoto:string = "my photo"
+
+  myImage:string = "assets/images/aleppo.jpg";
+  
+  displayImage: boolean = true;
+
+  listFilter: string = 'Leaf';
+
+  products: any[] = [
+    {
+      "productId": 1,
+      "productName": "abdo",
+      "productCode": "1cc",
+      "productAvai": "2.2.2022",
+      "productPrice": "500",
+      "productRating": "3.2",
+    },
+    {
+      "productId": 2,
+      "productName": "mobayed",
+      "productCode": "1cc",
+      "productAvai": "2.2.2022",
+      "productPrice": "500",
+      "productRating": "3.2",
+    },
+    {
+      "productId": 3,
+      "productName": "abdalah",
+      "productCode": "1cc",
+      "productAvai": "2.2.2022",
+      "productPrice": "500",
+      "productRating": "3.2",
+    },
+  ];
+
+  toggleImage(): void {
+    this.displayImage = !this.displayImage;
+    console.log('toggle button');
+  }
+}
